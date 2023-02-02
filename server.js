28079b248d5ddd42d5ccd53001aec055ddfc07d0
Nikkakko <nikolozkopadze@gmail.com>
2023-02-02 16:54:56 +0400

    cors updated

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const server = app.listen(PORT, () => {
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: '*',
+    origin: ['http://localhost:5173', 'https://realtime-chat-app.onrender.com'],
   },
 });
 
